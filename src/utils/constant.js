db57479235c5b2fe5bfeb0ec97de63b07eff33dc
Sunny Bhushan <sunnybhushan71@gmail.com>
2024-08-24 13:32:42 +0530

    first commit

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="https://back-byw5.onrender.com/api/v1/user";
-export const JOB_API_END_POINT="https://back-byw5.onrender.com/api/v1/job";
-export const APPLICATION_API_END_POINT="https://back-byw5.onrender.com/api/v1/application";
-export const COMPANY_API_END_POINT="https://back-byw5.onrender.com/api/v1/company";
+export const USER_API_END_POINT="https://back-1-wofp.onrender.com/api/v1/user";
+export const JOB_API_END_POINT="https://back-1-wofp.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT="https://back-1-wofp.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT="https://back-1-wofp.onrender.com/api/v1/company";
